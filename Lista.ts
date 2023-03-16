dfc47c07df2f62b860dc24c7975c602a20e36749
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2023-03-16 15:01:00 -0300

    lista impressao e remocao head

--- Lista.ts
+++ Lista.ts
@@ -1,7 +1,7 @@
 import { No } from "./No";
 
 export class Lista {
-	public totalNos: number = 0;
+	private totalNos: number = 0;
 	public head: No = null;
 
 	constructor(valor?) {
@@ -13,10 +13,36 @@ export class Lista {
 		//this.head = no;
 	}
 
-	public adicionarHead(valor) {
-		let novo = new No(valor);
-		this.head = novo;
+	public adicionarHead(valor: any) {
+		let novoNo = new No(valor);
+
+		let noTemporario = this.head;
+		this.head = novoNo;
+		novoNo.proximo = noTemporario;
+
 		this.totalNos++;
+	}
+
+	public removerHead() {
+		let noTemporario = this.head.proximo;
+		this.head = noTemporario;
+		this.totalNos--;
+	}
+
+	public imprimir(): void {
+		if (this.estarVazia()) {
+			console.log("> lista vazia");
+			return;
+		}
+
+		let elementoIteracao = this.head;
+		for (let index = 0; ; index++) {
+			console.log(`index: ${index}, elemento: ${elementoIteracao.valor}`);
+
+			if (elementoIteracao.proximo == null) break;
+
+			elementoIteracao = elementoIteracao.proximo;
+		}
 	}
 
 	public tamanho(): number {
--- index.ts
+++ index.ts
@@ -4,8 +4,18 @@ import { Lista } from "./Lista";
 
 const lista = new Lista("aluno01");
 lista.adicionarHead("aluno02");
+lista.adicionarHead("aluno03");
+lista.adicionarHead("aluno04");
+lista.adicionarHead("aluno05");
+lista.adicionarHead("aluno06");
+lista.adicionarHead("aluno07");
+lista.adicionarHead("aluno08");
+lista.adicionarHead("aluno09");
 
-console.log(lista.estarVazia());
-console.log(lista.tamanho());
-console.log(lista.head.valor);
-console.log(lista.head.proximo);
+console.log("lista vazia:", lista.estarVazia());
+console.log("tamanho:", lista.tamanho());
+// console.log("cabeca:", lista.head);
+// console.log("proximo elemento:", lista.head.proximo);
+
+lista.removerHead();
+lista.imprimir();
